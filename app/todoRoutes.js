303c00d4687643a1f3136d356113e54fbecb4fee
Ruben Vicario Gonzalez <ruben1586@gmail.com>
2015-04-10 23:10:01 +0200

    change file name for todo routes

--- app/todoRoutes.js
+++ app/todoRoutes.js
@@ -50,15 +50,6 @@ module.exports = function(app) {
 			getTodos(res);
 		});
 	});
-    
-  
-  app.get('/', passport.authenticate('github', { failureRedirect: '/login' }));
-
-	
-	app.get('/callback', passport.authenticate('github', { failureRedirect: '/login' }),
-	  function(req, res) {
-	    res.redirect('/home');
-	  });
 
 	// application -------------------------------------------------------------
 	app.get('*', function(req, res) {
