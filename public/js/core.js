ff2b143291c0321726c6806e3f0bbe20639ae4e3
Ruben Vicario Gonzalez <ruben1586@gmail.com>
2015-04-13 02:00:56 +0200

    implement ui router

--- public/js/core.js
+++ public/js/core.js
@@ -1,1 +1,22 @@
-angular.module('scotchTodo', ['todoController', 'todoService']);
+angular.module('scotchTodo', ['ui.router', 'todoController', 'todoService'])
+  .config(config);
+
+  function config($stateProvider, $locationProvider) {
+    $stateProvider
+      .state('login', {
+      	url: '/',
+        templateUrl: '/js/views/login.html',
+        controller: '',
+        controllerAs: 'vm'
+      })
+      .state('todos', {
+        url: '/tasks',
+        templateUrl: '/js/views/todos.html',
+        controller: 'mainController',
+        controllerAs: 'vm'
+      });
+
+
+    $locationProvider.html5Mode(true);
+  }
+
