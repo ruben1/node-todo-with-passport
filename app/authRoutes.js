ce352d954145dee20d4685106ee69cb4611ccdfe
Ruben Vicario Gonzalez <ruben1586@gmail.com>
2015-06-16 22:00:52 +0200

    add oauth routes

--- app/authRoutes.js
+++ app/authRoutes.js
@@ -1,11 +1,15 @@
-
+var passport = require('passport');
 
 module.exports = function(app) {
   //app.get('/', passport.authenticate('name-of-strategy', { failureRedirect: '/login' }));
-
+  app.get('/api/auth', passport.authenticate('github', { failureRedirect: '/login' }));
 
   //app.get('/callback', passport.authenticate('name-of-strategy', { failureRedirect: '/login' }),
     //function(req, res) {
       //res.redirect('/home');
     //});
+  app.get('/api/auth/callback', passport.authenticate('github', { successRedirect: '/', failureRedirect: '/login' }),
+    function(req, res) {
+      res.redirect('/home');
+    });
 };
